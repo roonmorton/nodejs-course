ab138f52b934e14ec32529a18062c98078079de1
Ron <roonmorton@gmail.com>
2019-07-18 10:26:17 -0600

    Update consumers.js

--- 9 - node-MySQL/src/app/routes/consumers.js
+++ 9 - node-MySQL/src/app/routes/consumers.js
@@ -4,9 +4,6 @@ const tableName = 'Consumer';
 module.exports = (app, mysql, model) => {
 
     app.get('/', (req, res) => {
-        
-
-        //for(i=0;i<1000;i++){
                 let connection = mysql.createConnection(dbConnection);
                 connection.connect();
                 let Customer = model(connection, tableName, 'idUser');
@@ -15,21 +12,13 @@ module.exports = (app, mysql, model) => {
                     .then(
                         result => {
                             console.log(result);
-                            //console.log("complete: " + complete);
                         })
                     .catch(
                         err => {
                             console.log(err.message);
-                            //console.log("error: " + error);
                         }
                     );
                 connection.end();
                 console.log("peticion GET");
-            //}
-        //console.log(benchmarkjs.results);
-        /*res.send({
-            complete: complete,
-            errro: error
-        });*/
     });
 };
